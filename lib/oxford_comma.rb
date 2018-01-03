5d305e05909f9f311f8c2a6069e759f5c0b9cd32
Kriti Rai <kriti.rai@gmail.com>
2018-01-03 05:39:11 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,10 @@
 def oxford_comma(array)
  last_item=array.pop
  string=array.join(", ")
- string << ", and " + last_item
+	if array.size >2
+		string << ", and " + last_item
+	else
+		string << " and " + last_item
+	end
+	
 end
