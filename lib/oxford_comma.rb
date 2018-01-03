9ffbc2cdf2ba0a348c9308c2f2ffaeb2b3c437b4
Kriti Rai <kriti.rai@gmail.com>
2018-01-03 05:41:15 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,9 @@
 def oxford_comma(array)
  last_item=array.pop
  string=array.join(", ")
-	if array.size =2
-		string << " and " + last_item
-	elsif array.size>2
+	if array.size >=3
+		string << ", and " + last_item
+	else
 		string << " and " + last_item
 	end
 
